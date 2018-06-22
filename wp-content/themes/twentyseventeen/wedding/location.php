8ed95cb702be64f3c7e19e9fd69befce3d2080e6
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2018-06-22 07:04:28 +0700

    update clickable address

--- wp-content/themes/twentyseventeen/wedding/location.php
+++ wp-content/themes/twentyseventeen/wedding/location.php
@@ -5,11 +5,15 @@
         <div class="devider">
             <img src="<?php echo get_template_directory_uri(); ?>/wedding/img/devider-flower.png" alt="devider">
         </div>
-        <p class="intro-text text-center">Click marker at map to show the location.</p>
+        <p class="intro-text text-center">Click marker at map or address below to show the location.</p>
         <div class="row">
             <div class="col-sm-4 col-sm-offset-4">
                 <div class="ev-name">Ceremony</div>
-                <p>Bride's house, Kp. Sukarena no. 26 RT 01 RW 20 Sukawening, Ciwidey</p>
+                <p>
+                    <a href="https://maps.google.com/maps?q=-7.071170,107.468259" target="_blank">
+                        Bride's house, Kp. Sukarena no. 26 RT 01 RW 20 Sukawening, Ciwidey
+                    </a>
+                </p>
             </div>
         </div>
     </div>
